0df322489638863910d79759a8c645b4fba38107
dpoluyanov <neiwick@icloud.com>
2021-12-10 16:14:09 +0300

    fix: workspace path prefixes

--- main.go
+++ main.go
@@ -18,7 +18,10 @@ package main
 import (
 	bep "bazel-wsl/bep"
 	"bazel-wsl/utils"
+	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -102,7 +105,7 @@ func main() {
 	//defer func() {
 	//	logFile.Close()
 	//}()
-	//mw := io.MultiWriter(os.Stdout, logFile)
+	//mw := io.MultiWriter(logFile, )
 	//logFileErr, err := os.OpenFile("C:\\Users\\la_d.poluyanov\\GolandProjects\\bazel\\log_err.txt", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 	//if err != nil {
 	//	panic(err)
@@ -112,11 +115,15 @@ func main() {
 	//}()
 	//mwErr := io.MultiWriter(os.Stderr, logFileErr)
 
+	var outBuffer bytes.Buffer
 	cmd := exec.Command("wsl", bazelArgs...)
-	cmd.Stdout = os.Stdout
+	cmd.Stdout = &outBuffer
 	cmd.Stderr = os.Stderr
 
 	err = cmd.Run()
+
+	var patchedBuffer = PatchBuffer(&outBuffer)
+	os.Stdout.Write(patchedBuffer.Bytes())
 
 	if err != nil {
 		fo.WriteString(fmt.Sprint(err))
@@ -150,4 +157,53 @@ func main() {
 
 		//time.Sleep(60 * time.Second)
 	}
+}
+
+func PatchBuffer(buf *bytes.Buffer) *bytes.Buffer {
+	var outBuffer bytes.Buffer
+
+	var reader = bufio.NewReader(buf)
+	var writer = bufio.NewWriter(&outBuffer)
+	var commonWSLPrefix = utils.WSLToWinPath("/home")
+	commonWSLPrefix = strings.ReplaceAll(commonWSLPrefix, "\\", "/")
+	commonWSLPrefix = strings.ReplaceAll(commonWSLPrefix, "/home", "")
+	var prefixes = make([]string, 0)
+	prefixes = append(prefixes, "bazel-bin: /home")
+	prefixes = append(prefixes, "bazel-genfiles: /home")
+	prefixes = append(prefixes, "bazel-testlogs: /home")
+	prefixes = append(prefixes, "command_log: /home")
+	prefixes = append(prefixes, "execution_root: /home")
+	prefixes = append(prefixes, "install_base: /home")
+	prefixes = append(prefixes, "java-home: /home")
+	prefixes = append(prefixes, "output_base: /home")
+	prefixes = append(prefixes, "repository_cache: /home")
+	prefixes = append(prefixes, "server_log: /home")
+	prefixes = append(prefixes, "workspace: /home")
+	for {
+		var line, err = reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				panic(err)
+			}
+		}
+		for _, prefix := range prefixes {
+			if strings.HasPrefix(line, prefix) {
+				var index = strings.Index(line, ": ")
+				left := line[0:index]
+				line = left + ": " + commonWSLPrefix + line[index+2:]
+			}
+		}
+
+		_, err = writer.WriteString(line + "\n")
+		if err != nil {
+			panic(err)
+		}
+	}
+	err := writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+	return &outBuffer
 }
